aabba37c0d319f69d5b6f19ace7fb4975b1ed9bd
Bassem Girgis <bassem.girgis@bp.com>
2020-10-23 12:23:56 -0500

    update setup

--- setup.py
+++ setup.py
@@ -65,6 +65,27 @@ packages = [
     'zmqrpc.server',
 ]
 
+classifiers = [
+    # How mature is this project? Common values are
+    #   3 - Alpha
+    #   4 - Beta
+    #   5 - Production/Stable
+    'Development Status :: 3 - Alpha',
+
+    # Indicate who your project is intended for
+    'Intended Audience :: Developers',
+
+    'Topic :: Internet :: WWW/HTTP :: Site Management',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+
+    'License :: OSI Approved :: MIT License (MIT)',
+
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+]
+
 setup(
     name=name,
     version=version,
@@ -80,4 +101,5 @@ setup(
     packages=packages,
     include_package_data=True,
     install_requires=install_requires,
+    classifiers=classifiers,
 )
